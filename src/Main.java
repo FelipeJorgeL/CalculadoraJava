414960d0cd9a7d45165f342b673510a82dadc3f1
Felipe Jorge Lopes <felipejorge-ieg@instituto.ad>
2025-06-02 09:27:42 -0300

    fazendo o toString

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Metodos metodos = new Metodos();
+
+        System.out.println(metodos);
+
 
     }
 }
--- src/Metodos.java
+++ src/Metodos.java
@@ -2,4 +2,32 @@ public class Metodos {
     public static double adicionar(double x, double y) {
         return x + y;
     }
+
+    public static double subtrair(double x, double y) {
+        return x - y;
+    }
+
+    public static double multiplicar(double x, double y) {
+        return x * y;
+    }
+
+    public static double dividir(double x, double y) {
+        return x / y;
+    }
+
+    public static double potencia(double x, double y) {
+        return Math.pow(x, y); //Metodo que faz potenciação
+    }
+
+    public static double raizQuadrada(double x) {
+        return Math.sqrt(x);
+    }
+
+    public String toString() {
+        return "CALCULADORA " +
+                "\n7 | 8 | 9 | x" +
+                "\n4 | 5 | 6 | -" +
+                "\n1 | 2 | 3 | +" +
+                "\n  | 0 |  ";
+    }
 }
